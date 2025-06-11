2a03dc4366d9376f94cd63aed324ded400336cac
Advaitgaur004 <b22cs004@iitj.ac.in>
2025-06-12 00:02:29 +0530

    Extended Test Case for Sum Operator

--- tests/Operator/test_sum.c
+++ tests/Operator/test_sum.c
@@ -65,5 +65,87 @@ void test_sum_operator() {
         compare_tensors(&actual_res, &expected_res, op_name, tc_name, 1, TEST_FLOAT_TOLERANCE);
     }
 
+    // Test Case 5: Large Tensor Reductions
+    {
+        const char* tc_name = "sum_large_tensor_reductions";
+        
+        // Sub-test 1: Large tensor sum (1,000 elements)
+        {
+            TensorShape large_shape = {1000, 0, 0, 0};
+            float large_data[1000];
+            for(int i = 0; i < 1000; i++) large_data[i] = 1.0f;
+            
+            float exp_d[] = {1000.0f}; // Sum of 1000 ones
+            TensorShape exp_shape = {1, 0, 0, 0};
+            
+            Tensor t1 = create_test_tensor(large_shape, large_data, false);
+            Tensor expected_res = create_test_tensor(exp_shape, exp_d, false);
+            Tensor actual_res = Tensor_sum(t1);
+
+            compare_tensors(&actual_res, &expected_res, op_name, tc_name, 1, TEST_FLOAT_TOLERANCE);
+        }
+
+        // Sub-test 2: Larger tensor sum (stress test - 5,000 elements)
+        {
+            TensorShape stress_shape = {5000, 0, 0, 0};
+            float stress_data[5000];
+            for(int i = 0; i < 5000; i++) stress_data[i] = 1.0f;
+            
+            float exp_d[] = {5000.0f}; // Sum of 5000 ones
+            TensorShape exp_shape = {1, 0, 0, 0};
+            
+            Tensor t1 = create_test_tensor(stress_shape, stress_data, false);
+            Tensor expected_res = create_test_tensor(exp_shape, exp_d, false);
+            Tensor actual_res = Tensor_sum(t1);
+
+            compare_tensors(&actual_res, &expected_res, op_name, tc_name, 2, TEST_FLOAT_TOLERANCE);
+        }
+    }
+
+    // Test Case 6: Higher Dimensional Tensors
+    {
+        const char* tc_name = "sum_higher_dimensional_tensors";
+        
+        // Sub-test 1: 3D tensor sum
+        {
+            TensorShape shape_3d = {2, 3, 4, 0};
+            float d1[24];
+            float sum = 0.0f;
+            for(int i = 0; i < 24; i++) {
+                d1[i] = (float)(i + 1);
+                sum += d1[i];
+            }
+            
+            float exp_d[] = {sum}; // Sum of 1+2+...+24 = 300
+            TensorShape exp_shape = {1, 0, 0, 0};
+            
+            Tensor t1 = create_test_tensor(shape_3d, d1, false);
+            Tensor expected_res = create_test_tensor(exp_shape, exp_d, false);
+            Tensor actual_res = Tensor_sum(t1);
+
+            compare_tensors(&actual_res, &expected_res, op_name, tc_name, 1, TEST_FLOAT_TOLERANCE);
+        }
+
+        // Sub-test 2: 4D tensor sum
+        {
+            TensorShape shape_4d = {2, 3, 4, 5};
+            float d1[120];
+            float sum = 0.0f;
+            for(int i = 0; i < 120; i++) {
+                d1[i] = (float)(i + 1);
+                sum += d1[i];
+            }
+            
+            float exp_d[] = {sum}; // Sum of 1+2+...+120 = 7260
+            TensorShape exp_shape = {1, 0, 0, 0};
+            
+            Tensor t1 = create_test_tensor(shape_4d, d1, false);
+            Tensor expected_res = create_test_tensor(exp_shape, exp_d, false);
+            Tensor actual_res = Tensor_sum(t1);
+
+            compare_tensors(&actual_res, &expected_res, op_name, tc_name, 2, TEST_FLOAT_TOLERANCE);
+        }
+    }
+
     cten_free(pool_id);
 }
