ad717e8cce00193772f49922bc2b100e5298cfc2
Advaitgaur004 <b22cs004@iitj.ac.in>
2025-06-14 07:04:52 +0530

    added declaration of marcos. fir _CTEN_PICK for tensor_mean and tensor_sum

--- include/cten.h
+++ include/cten.h
@@ -4,6 +4,12 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdarg.h>
+#include <limits.h>
+
+#define _CTEN_PICK(_1,_2,NAME,...) NAME
+#define Tensor_mean(...) _CTEN_PICK(__VA_ARGS__, Tensor_mean_dim, Tensor_mean_all)(__VA_ARGS__)
+#define Tensor_sum(...)  _CTEN_PICK(__VA_ARGS__, Tensor_sum_dim,  Tensor_sum_all )(__VA_ARGS__)
 
 typedef int TensorShape[4];
 typedef struct GradNode GradNode;
@@ -67,8 +73,12 @@ Tensor Tensor_matmul(Tensor self, Tensor other);
 Tensor Tensor_neg(Tensor self);
 Tensor Tensor_abs(Tensor self);
 
-Tensor Tensor_sum(Tensor self,int dim);
-Tensor Tensor_mean(Tensor self,int dim);
+/* Helper functions that the macros dispatch to */
+Tensor Tensor_mean_all(Tensor self);
+Tensor Tensor_mean_dim(Tensor self, int dim);
+Tensor Tensor_sum_all (Tensor self);
+Tensor Tensor_sum_dim (Tensor self, int dim);
+
 Tensor Tensor_max(Tensor self);
 Tensor Tensor_min(Tensor self);
 
@@ -111,6 +121,7 @@ void optim_sgd_delete(optim_sgd* self);
 void cten_begin_eval();
 bool cten_is_eval();
 void cten_end_eval();
+bool va_arg_is_present(va_list args);
 
 /* Utils */
 void Tensor_normalize_dataset(const float (*X)[4], float (*X_norm)[4], int n_samples, int n_train_samples, int n_features);Tensor Tensor_detach(Tensor self);
