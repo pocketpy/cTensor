0c117a5cd415cb6233dca015b4c271d6ffc56982
Advaitgaur004 <b22cs004@iitj.ac.in>
2025-07-23 22:08:45 +0530

    Cherry picked from operator-6 :ec9b7f86755cc0446b3f7f69fc8ec0b166bf9f5e
    - which essentially Add Tensor_abs implementation

--- tests/cten_tests.c
+++ tests/cten_tests.c
@@ -27,6 +27,7 @@ void test_square_operator();
 void test_div_operator();
 void test_max_operator();
 void test_min_operator();
+void test_abs_operator();
 
 // Backward tests
 void test_add_backward();
@@ -41,6 +42,7 @@ void test_sum_backward();
 void test_mean_backward();
 void test_div_backward();
 void test_pow_backward();
+void test_abs_backward();
 
 int main() {
     printf("Starting cTensor Test Suite on %s...\n", PLATFORM_NAME);
@@ -110,6 +112,9 @@ int main() {
     test_min_operator();
     printf("Min operator tests finished.\n");
 
+    test_abs_operator();
+    printf("Abs operator tests finished.\n");
+
     // Backward tests
     test_add_backward();
     printf("Add backward tests finished.\n");
@@ -146,6 +151,10 @@ int main() {
     
     test_pow_backward();
     printf("Pow backward tests finished.\n");
+    
+    test_abs_backward();
+    printf("Abs backward tests finished.\n");
+    
     // other tests
     
     csv_reporter_close();
