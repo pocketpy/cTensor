47337ca3815c8b76b3c1e912feefebf5ebaed672
Advaitgaur004 <b22cs004@iitj.ac.in>
2025-08-02 15:52:21 +0530

    AdaGrad Optimizer added

--- src/optimizer/sgd.c
+++ src/optimizer/sgd.c
@@ -1,6 +1,6 @@
 #include "cten.h"
 #include "cten_internal.h"
-
+#include <math.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
@@ -38,6 +38,47 @@ void optim_sgd_step(optim_sgd* self) {
         // step
         for(int j = 0; j < t.data->numel; j++) {
             t.data->flex[j] -= self->lr * t.node->grad.data->flex[j];
+        }
+    }
+}
+
+typedef struct optim_adagrad {
+    int n_params;
+    Tensor* params;
+    float lr;
+    float ε;
+    Tensor* sum_sq_grad;
+} optim_adagrad;
+
+optim_adagrad* optim_adagrad_new(int n_params, Tensor* params, float lr, float ε) {
+    optim_adagrad* self = _cten_malloc(sizeof(optim_adagrad));
+    self->n_params = n_params;
+    self->params = params;
+    self->lr = lr;
+    self->ε = ε;
+    self->sum_sq_grad = _cten_malloc(sizeof(Tensor) * n_params);
+    for (int i = 0; i < n_params; i++) {
+        self->sum_sq_grad[i] = Tensor_zeros(params[i].shape, false);
+    }
+    return self;
+}
+
+void optim_adagrad_zerograd(optim_adagrad* self) {
+    _cten_zero_grad(self->params, self->n_params);
+}
+
+void optim_adagrad_step(optim_adagrad* self) {
+    for (int i = 0; i < self->n_params; i++) {
+        Tensor t = self->params[i];
+        if (t.node == NULL || t.node->grad.data == NULL) continue;
+
+        Tensor grad = t.node->grad;
+        Tensor* sum_sq = &self->sum_sq_grad[i];
+
+        for (int j = 0; j < t.data->numel; j++) {
+            float g = grad.data->flex[j];
+            sum_sq->data->flex[j] += g * g;
+            t.data->flex[j] -= self->lr * g / (sqrtf(sum_sq->data->flex[j]) + self->ε);
         }
     }
 }
