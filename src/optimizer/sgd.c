5285d2df35e569f2e499f6dc3f633d634e4c0dfa
Advaitgaur004 <b22cs004@iitj.ac.in>
2025-08-02 15:53:41 +0530

    RMSPROP optimizer added

--- src/optimizer/sgd.c
+++ src/optimizer/sgd.c
@@ -82,3 +82,47 @@ void optim_adagrad_step(optim_adagrad* self) {
         }
     }
 }
+
+typedef struct optim_rmsprop {
+    int n_params;
+    Tensor* params;
+    float lr;
+    float β;
+    float ε;
+    Tensor* squared_avg;
+} optim_rmsprop;
+
+optim_rmsprop* optim_rmsprop_new(int n_params, Tensor* params, float lr, float β, float ε) {
+    optim_rmsprop* self = _cten_malloc(sizeof(optim_rmsprop));
+    self->n_params = n_params;
+    self->params = params;
+    self->lr = lr;
+    self->β = β;
+    self->ε = ε;
+
+    self->squared_avg = _cten_malloc(sizeof(Tensor) * n_params);
+    for (int i = 0; i < n_params; i++) {
+        self->squared_avg[i] = Tensor_zeros(params[i].shape, false);
+    }
+    return self;
+}
+
+void optim_rmsprop_zerograd(optim_rmsprop* self) {
+    _cten_zero_grad(self->params, self->n_params);
+}
+
+void optim_rmsprop_step(optim_rmsprop* self) {
+    for (int i = 0; i < self->n_params; i++) {
+        Tensor t = self->params[i];
+        if (t.node == NULL || t.node->grad.data == NULL) continue;
+
+        Tensor grad = t.node->grad;
+        Tensor* sq_avg = &self->squared_avg[i];
+
+        for (int j = 0; j < t.data->numel; j++) {
+            float g = grad.data->flex[j];
+            sq_avg->data->flex[j] = self->β * sq_avg->data->flex[j] + (1 - self->β) * g * g;
+            t.data->flex[j] -= self->lr * g / (sqrtf(sq_avg->data->flex[j]) + self->ε);
+        }
+    }
+}
